a890175077aae05e443301ff818a0f63d0f28a82
Austin Powell <austinpowell58@gmail.com>
2018-08-01 01:20:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
 WIN_COMBINATIONS.each do |win_combo|
   if win_combo[0] == "X" && [win_combo[1] == "X"  && win_combo[2] == "X"
     return win_combo
-  elsif board[win_combo[0]] == "O" && board[win_combo[1]] == "O"  && board[win_combo[2]] == "O"
+  elsif win_combo[0] == "O" && win_combo[1] == "O"  && win_combo[2]== "O"
     return win_combo
   end 
   return FALSE
